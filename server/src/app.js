e741ea3969f109a1602e4b1eeda5a58f055c7118
Trung78z <trungpyy@gmail.com>
2024-06-15 21:16:51 +0700

    create-project

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(cors({ origin: true, optionsSuccessStatus: 200, credentials: true }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to api!" });
+});
+
+export default app;
