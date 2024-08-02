e3edbb6524df9bd74a4a821bc6141f2ba6a66ea7
Trung78z <trungpspy@gmail.com>
2024-08-02 14:23:38 +0700

    .

--- admin/src/configs/api.js
+++ admin/src/configs/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// export const API_URL = "http://localhost:8080";
-export const API_URL = "https://api.taphoazalo.com";
+export const API_URL = "http://localhost:8080";
+// export const API_URL = "https://api.taphoazalo.com";
 export default axios.create({
   baseURL: `${API_URL}/api`,
   timeout: 1000,
